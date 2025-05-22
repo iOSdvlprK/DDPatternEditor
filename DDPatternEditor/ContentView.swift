e9a6fff789591ea80fb1241828e7e59c3327ee61
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-05-22 23:23:24 +0900

    set up the pattern editor

--- DDPatternEditor/ContentView.swift
+++ DDPatternEditor/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    let n: Int
+    @State private var name: String = "pattern"
+    @State private var pattern: [[Bool]]
+    
+    init(n: Int) {
+        self.n = n
+        self._pattern = State(
+            initialValue: Array(
+                repeating: Array(
+                    repeating: false,
+                    count: n
+                ),
+                count: n
+            )
+        )
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            PatternEditor()
+            
+            CodePreview()
         }
         .padding()
+        .frame(minWidth: 800, minHeight: 600)
+    }
+}
+
+struct PatternEditor: View {
+    var body: some View {
+        // TODO: pattern editor
+        Text("Pattern Editor")
+    }
+}
+
+struct CodePreview: View {
+    var body: some View {
+        // TODO: code preview
+        Text("Code Preview")
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(n: 25)
 }
