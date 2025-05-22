171a15fdda2cd86b9b13080673ed7355c2abfc3a
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-05-22 22:49:03 +0900

    Initial Commit

---
+++ DDPatternEditor/DDPatternEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDPatternEditorApp.swift
+//  DDPatternEditor
+//
+//  Created by joe on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct DDPatternEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
