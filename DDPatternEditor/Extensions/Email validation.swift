6c2db02f1fee218c9935c400ff3601ec9abd0787
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-05-25 23:06:22 +0900

    string extension: identifier validation

--- DDPatternEditor/Extensions/Email validation.swift
+++ DDPatternEditor/Extensions/Email validation.swift
@@ -10,12 +10,18 @@ extension String {
         // non-empty: the string must be non-empty
         guard !self.isEmpty else { return false }
         
+        // starts with a letter or underscore
+        let firstCharacter = self.first!
+        if !firstCharacter.isLetter && firstCharacter != "_" {
+            return false
+        }
+        
+        // remaining characters is a letter, digit or underscore
+        let validCharacters = self
+            .allSatisfy { $0.isLetter || $0.isNumber || $0 == "_" }
+        if !validCharacters { return false }
+        
+        // self is a valid identifier!
         return true
-        
-        // TODO: test for:
-        // 1. not a Swift keyword
-        // 2. starts with a letter or underscore
-        // 3. remaining characters is a letter, digit or underscore
-        
     }
 }
